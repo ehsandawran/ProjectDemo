94e4cbdf8aca0957048f67029f56e933e6b7de89
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-04 00:32:40 -0500

    added Thread.sleep

--- src/demopackage/DemoClass.java
+++ src/demopackage/DemoClass.java
@@ -1,27 +1,28 @@
 package demopackage;
 
 public class DemoClass {
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Demo Print");
-		
+
 		System.out.println("Sprint Demo1");
 		System.out.println("Sprint Demo2");
-		
-	DemoClass obj = new DemoClass();
+
+		DemoClass obj = new DemoClass();
 
 		obj.demoLoop();
-	
+
 	}
 
-	public void demoLoop(){
+	public void demoLoop() throws InterruptedException {
 
+		for (int i = 0; i < 100; i++) {
+			System.out.println("This is a demo loop...");
+			
+			Thread.sleep(100);
 
-		for(int i = 0; i < 100; i++){
-		System.out.println("This is a demo loop...");
-		
 		}
-		
+
 	}
 
 }
