dd4925791d09d92c52f2cfd07428976881bc6946
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-04 00:18:20 -0500

    Demo Commit

---
+++ src/demopackage/DemoClass.java
@@ -0,0 +1,13 @@
+package demopackage;
+
+public class DemoClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Demo Print");
+		
+		System.out.println("Sprint Demo1");
+		System.out.println("Sprint Demo2");
+		
+	}
+
+}
