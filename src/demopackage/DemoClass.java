c3b327d17333217ea8353658e89fb525dc3cf5a0
ehsandawran <155399368+ehsandawran@users.noreply.github.com>
2024-01-04 00:22:01 -0500

    Update DemoClass.java

--- src/demopackage/DemoClass.java
+++ src/demopackage/DemoClass.java
@@ -8,6 +8,20 @@ public class DemoClass {
 		System.out.println("Sprint Demo1");
 		System.out.println("Sprint Demo2");
 		
+	DemoClass obj = new DemoClass();
+
+		obj.demoLoop();
+	
+	}
+
+	public void demoLoop(){
+
+
+		for(int i = 0; i < 100; i++){
+		System.out.println("This is a demo loop...");
+		
+		}
+		
 	}
 
 }
